fff9143a57c8b6c3d9721dcb214519c4d5ae87c9
ToroSamy <1364596766@qq.com>
2024-08-22 01:02:30 +0800

    Create README.md and LICENSE

---
+++ README.md
@@ -0,0 +1,11 @@
+# 正在制作当中
+
+
+## Contact Author
+
+- qq: 1364596766
+- website: https://www.torosamy.net
+
+## License
+
+[MIT](./LICENSE)
---
+++ src/main/kotlin/net/torosamy/torosamyItem/TorosamyItem.kt
@@ -0,0 +1,14 @@
+package net.torosamy.torosamyItem
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class TorosamyItem : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
